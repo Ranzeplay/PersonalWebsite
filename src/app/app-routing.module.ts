ccb1324d569be748c841edc02c69107624b3087c
Jeb Feng <ranzeplay@outlook.com>
2021-08-13 23:06:30 +0800

    Update site name & Add angular-cli-ghpages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,22 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from '../../products/products.component';
 
-
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'products', component: ProductsComponent }
+  {
+    path: 'home',
+    component: HomeComponent,
+    data: { title: 'Home - Jeb Feng' },
+  },
+  {
+    path: 'products',
+    component: ProductsComponent,
+    data: { title: 'Products - Jeb Feng' },
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
