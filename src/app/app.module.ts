8d4b8212a3758ee2157ba6aca3d36cb8963ca628
Jeb Feng <ranzeplay@outlook.com>
2021-08-14 10:35:37 +0800

    Update project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { ProductsComponent } from '../../products/products.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
